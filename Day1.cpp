6827063710bdc617e8597cb4624b4b720e483da1
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-11 20:15:37 +0530

    Create Day1.cpp
    Easy

---
+++ Day1.cpp
@@ -0,0 +1,31 @@
+/* 3330. Find the Original Typed String I :
+
+Alice is attempting to type a specific string on her computer. However, she tends to be clumsy and may press a key for too long, resulting in a character being typed multiple times.
+Although Alice tried to focus on her typing, she is aware that she may still have done this at most once.
+You are given a string word, which represents the final output displayed on Alice's screen.
+Return the total number of possible original strings that Alice might have intended to type. */
+
+
+class Solution {
+public:
+// 1
+    int possibleStringCount(string word) {
+        int total = 1; // keep word as is
+        int n = word.size();
+        int i = 0;
+        
+        while (i < n) {
+            int j = i;
+            while (j < n && word[j] == word[i]) {
+                j++;
+            }
+            int groupLength = j - i;
+            if (groupLength >= 2) {
+                total += (groupLength - 1);
+            }
+            i = j;
+        }
+        
+        return total;
+    }
+};
