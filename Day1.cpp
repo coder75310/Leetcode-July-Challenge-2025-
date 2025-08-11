d4a049e63e6fc9f1e408f92cee11146edd67316d
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-11 20:16:07 +0530

    Update Day1.cpp

--- Day1.cpp
+++ Day1.cpp
@@ -1,4 +1,4 @@
-/* 3330. Find the Original Typed String I :
+/* 3330. Find the Original Typed String I : Easy
 
 Alice is attempting to type a specific string on her computer. However, she tends to be clumsy and may press a key for too long, resulting in a character being typed multiple times.
 Although Alice tried to focus on her typing, she is aware that she may still have done this at most once.
