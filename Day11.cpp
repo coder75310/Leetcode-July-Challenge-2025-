6b0eb8d559cb6bcfc77f6e0e0d6d5cd5625ca512
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-16 21:58:02 +0530

    Update Day11.cpp

--- Day11.cpp
+++ Day11.cpp
@@ -1,4 +1,4 @@
-2402. Meeting Rooms III :
+/* 2402. Meeting Rooms III :
 You are given an integer n. There are n rooms numbered from 0 to n - 1.
 
 You are given a 2D integer array meetings where meetings[i] = [starti, endi] means that a meeting will be held during the half-closed time
@@ -12,4 +12,4 @@ as the original meeting.
 When a room becomes unused, meetings that have an earlier original start time should be given the room.
 Return the number of the room that held the most meetings. If there are multiple rooms, return the room with the lowest number.
 
-A half-closed interval [a, b) is the interval between a and b including a and not including b.
+A half-closed interval [a, b) is the interval between a and b including a and not including b. */
