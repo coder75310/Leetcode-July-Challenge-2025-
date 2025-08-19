3717697b16ae1607f4237d545b0b15a0163fafab
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-19 18:20:48 +0530

    Update Day14.cpp

--- Day14.cpp
+++ Day14.cpp
@@ -5,3 +5,16 @@ The linked list holds the binary representation of a number.
 Return the decimal value of the number in the linked list.
 
 The most significant bit is at the head of the linked list. */
+
+
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int result = 0;
+        while (head != nullptr) {
+            result = (result << 1) | head->val; // shift left and add current value
+            head = head->next;
+        }
+        return result;
+    }
+};
