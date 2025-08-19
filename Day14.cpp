f7ade49e47bed7507bc999c3026e6cf84beb6083
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-19 18:21:09 +0530

    Update Day14.cpp

--- Day14.cpp
+++ Day14.cpp
@@ -7,6 +7,16 @@ Return the decimal value of the number in the linked list.
 The most significant bit is at the head of the linked list. */
 
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     int getDecimalValue(ListNode* head) {
