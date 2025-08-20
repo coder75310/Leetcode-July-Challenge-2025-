aaff99bd2b35ae4d078820b920a2e38cbbf55f14
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-20 19:58:46 +0530

    Update Day15.cpp

--- Day15.cpp
+++ Day15.cpp
@@ -10,4 +10,30 @@ You are given a string word.
 Return true if word is valid, otherwise, return false. */
 
 
-  
+class Solution {
+public:
+    bool isValid(string word) {
+        if (word.size() < 3) return false;
+
+        bool hasVowel = false;
+        bool hasConsonant = false;
+
+        string vowels = "aeiouAEIOU";
+
+        for (char c : word) {
+            if (!isalnum(c)) {
+                // contains character other than digit or English letter
+                return false;
+            }
+            if (isalpha(c)) {
+                if (vowels.find(c) != string::npos) {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            }
+        }
+
+        return hasVowel && hasConsonant;
+    }
+};  
