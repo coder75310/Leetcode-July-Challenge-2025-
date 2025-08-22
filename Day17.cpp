fa757f3ad5f22ffeaf7cda0fc6d1b76a96149bd6
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-22 21:39:20 +0530

    Update Day17.cpp

--- Day17.cpp
+++ Day17.cpp
@@ -6,3 +6,22 @@ A subsequence sub of nums with length x is called valid if it satisfies:
 Return the length of the longest valid subsequence of nums. */
 
 
+class Solution {
+public:
+    int maximumLength(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<unordered_map<int, int>> dp(n); // dp[i][mod] = max length ending at i with mod
+        int result = 1; // min valid subsequence has at least length 1
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                int mod = (nums[j] + nums[i]) % k;
+                int prevLen = dp[j].count(mod) ? dp[j][mod] : 1;
+                dp[i][mod] = max(dp[i][mod], prevLen + 1);
+                result = max(result, dp[i][mod]);
+            }
+        }
+
+        return result;
+    }
+};
