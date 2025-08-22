17d2d994ea752433d5f972e8cba9a149b46f7b1b
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-22 21:39:09 +0530

    Update Day17.cpp

--- Day17.cpp
+++ Day17.cpp
@@ -4,3 +4,5 @@ A subsequence sub of nums with length x is called valid if it satisfies:
 
 (sub[0] + sub[1]) % k == (sub[1] + sub[2]) % k == ... == (sub[x - 2] + sub[x - 1]) % k.
 Return the length of the longest valid subsequence of nums. */
+
+
