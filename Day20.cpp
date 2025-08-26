468edbc154f4dc3e1923bf123d28b6c9624f6eef
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-26 20:22:17 +0530

    Update Day20.cpp

--- Day20.cpp
+++ Day20.cpp
@@ -1,4 +1,4 @@
-1948. Delete Duplicate Folders in System :
+/* 1948. Delete Duplicate Folders in System :
 Due to a bug, there are many duplicate folders in a file system. You are given a 2D array paths, where paths[i] is an array representing an absolute path to the ith folder in the file system.
 
 For example, ["one", "two", "three"] represents the path "/one/two/three".
@@ -18,5 +18,5 @@ However, if the file structure also included the path "/b/w", then the folders "
 would still be considered identical even with the added folder.
 Once all the identical folders and their subfolders have been marked, the file system will delete all of them. The file system only runs the deletion once, 
 so any folders that become identical after the initial deletion are not deleted.
-
+ */
 Return the 2D array ans containing the paths of the remaining folders after deleting all the marked folders. The paths may be returned in any order.
