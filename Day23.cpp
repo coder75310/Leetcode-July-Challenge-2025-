68d5c6c961b0c30c2d6aa0cba4e9146461b78044
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-30 18:31:37 +0530

    Update Day23.cpp

--- Day23.cpp
+++ Day23.cpp
@@ -6,3 +6,47 @@ For example, when removing "ab" from "cabxbae" it becomes "cxbae".
 Remove substring "ba" and gain y points.
 For example, when removing "ba" from "cabxbae" it becomes "cabxe".
 Return the maximum points you can gain after applying the above operations on s. */
+
+
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        int n     = s.length();
+        int score = 0;
+
+        string maxStr = (x > y) ? "ab" : "ba";
+        string minStr = (maxStr == "ab") ? "ba" : "ab"; //This is updated after the video was made as a new test case was added in Leetcode
+
+        //First Pass
+        string temp_first     = removeSubstring(s, maxStr);
+        int L                 = temp_first.length();
+        int removedPairsCount = (n - L) / 2;
+        score                += removedPairsCount * max(x, y);
+
+
+        //Second Pass
+        string temp_second = removeSubstring(temp_first, minStr);
+        removedPairsCount  = (L - temp_second.length()) / 2;
+        score             += removedPairsCount * min(x, y);
+
+        return score;
+    }
+
+    string removeSubstring(string& inputString, string& matchStr) {
+        int i = 0;
+
+        for (int j = 0; j < inputString.size(); j++) {
+            inputString[i++] = inputString[j];
+
+            if (i > 1 &&
+                inputString[i - 2] == matchStr[0] &&
+                inputString[i - 1] == matchStr[1]) {
+                i -= 2;
+            }
+        }
+
+        inputString.erase(inputString.begin() + i, inputString.end());
+
+        return inputString;
+    }
+};
