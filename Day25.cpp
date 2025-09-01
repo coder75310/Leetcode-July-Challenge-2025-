adf6e49d44a8656ca9f144fb742d19e7e0e70849
Ankit Singh <2023pcecsankit019@poornima.org>
2025-09-01 21:25:43 +0530

    Update Day25.cpp

--- Day25.cpp
+++ Day25.cpp
@@ -4,3 +4,10 @@ Code
 
 
 3487. Maximum Unique Subarray Sum After Deletion:
+You are given an integer array nums.
+
+You are allowed to delete any number of elements from nums without making it empty. After performing the deletions, select a subarray of nums such that:
+
+All elements in the subarray are unique.
+The sum of the elements in the subarray is maximized.
+Return the maximum sum of such a subarray.
