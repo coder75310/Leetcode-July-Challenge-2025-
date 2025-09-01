2bcf486bea581068aee52fd5dd37861e67a00e6a
Ankit Singh <2023pcecsankit019@poornima.org>
2025-09-01 21:26:18 +0530

    Update Day25.cpp

--- Day25.cpp
+++ Day25.cpp
@@ -11,3 +11,59 @@ You are allowed to delete any number of elements from nums without making it emp
 All elements in the subarray are unique.
 The sum of the elements in the subarray is maximized.
 Return the maximum sum of such a subarray. */
+
+
+// Method 1
+/* class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        unordered_set<int> st;
+        // vector<int> st(101, -1);
+
+        int sum = 0;
+
+        int maxNeg = INT_MIN;
+        
+        for(int &num : nums) {
+            if(num <= 0) {
+                maxNeg = max(maxNeg, num);
+            } else if(!st.count(num)) {
+                sum += num;
+                st.insert(num);
+            }
+        }
+        
+        return sum == 0 ? maxNeg : sum;
+    }
+};
+ */
+
+
+
+
+
+// Method 2
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        // unordered_set<int> st;
+        vector<int> st(101, -1);
+
+        int sum = 0;
+
+        int maxNeg = INT_MIN;
+
+        for(int &num : nums) {
+            if(num <= 0) {
+                maxNeg = max(maxNeg, num);
+            } else if(st[num] == -1) {
+                sum += num;
+                // st.insert(num);
+                st[num] = 1;
+            }
+        }
+        
+        return sum == 0 ? maxNeg : sum;
+    }
+};
+
