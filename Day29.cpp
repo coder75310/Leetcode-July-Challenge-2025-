440fcbc0e0cf28a1d679538f58cf1d0a3453cb5c
Ankit Singh <2023pcecsankit019@poornima.org>
2025-09-03 21:32:31 +0530

    Update Day29.cpp

--- Day29.cpp
+++ Day29.cpp
@@ -1,4 +1,4 @@
-2411. Smallest Subarrays With Maximum Bitwise OR:
+/* 2411. Smallest Subarrays With Maximum Bitwise OR:
 You are given a 0-indexed array nums of length n, consisting of non-negative integers. For each index i from 0 to n - 1, you must determine the size of the minimum sized non-empty
 subarray of nums starting at i (inclusive) that has the maximum possible bitwise OR.
 
@@ -8,4 +8,4 @@ The bitwise OR of an array is the bitwise OR of all the numbers in it.
 
 Return an integer array answer of size n where answer[i] is the length of the minimum sized subarray starting at i with maximum bitwise OR.
 
-A subarray is a contiguous non-empty sequence of elements within an array.
+A subarray is a contiguous non-empty sequence of elements within an array. */
