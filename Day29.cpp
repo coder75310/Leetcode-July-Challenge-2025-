2300001852d1dd1a5511b2e5682902f273858047
Ankit Singh <2023pcecsankit019@poornima.org>
2025-09-03 21:33:04 +0530

    Update Day29.cpp

--- Day29.cpp
+++ Day29.cpp
@@ -9,3 +9,88 @@ The bitwise OR of an array is the bitwise OR of all the numbers in it.
 Return an integer array answer of size n where answer[i] is the length of the minimum sized subarray starting at i with maximum bitwise OR.
 
 A subarray is a contiguous non-empty sequence of elements within an array. */
+
+
+/* class Solution {
+public:
+    vector<int> smallestSubarrays(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> result(n);
+        
+        vector<int> setBitIndex(32, -1);
+        //setBitIndex[j] = i, it means that jth bit can be set via element at index i in nums
+
+        for(int i = n-1; i >= 0; i--) {
+
+            //nums[i] = binary rep
+            int endIndex = i;
+            for(int j = 0; j < 32; j++) {
+                if(!(nums[i] & (1 << j))) { //if jth bit is not set
+                    if(setBitIndex[j] != -1) {
+                        endIndex = max(endIndex, setBitIndex[j]);
+                    }
+                } else {
+                    setBitIndex[j] = i;
+                }
+            }
+
+            result[i] = endIndex - i + 1;
+        }
+
+        return result;
+    }
+}; */
+
+
+
+
+
+// class Solution {
+//     public:
+//     vector<int> smallestSubarrays(vector<int>& nums){
+//         int last[32] = {};
+//         for(int i=nums.size()-1; i>=0; i--){
+//             int maxIndex = i;
+//             for(int b=0; b<32; b++){
+//                 if(nums[i] & (1<<b)){
+//                     last[b] = i;
+//                 } else {
+//                     maxIndex = max(maxIndex, last[b]);
+//                 }
+//             }
+//             nums[i] = maxIndex - i + 1;
+//         }
+//         return nums;
+//     }
+// };
+
+
+
+
+
+class Solution {
+public:
+    vector<int> smallestSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> lastSeen(32, 0); // to store the last seen index for each bit
+        vector<int> ans(n);
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int bit = 0; bit < 32; bit++) {
+                if ((nums[i] >> bit) & 1) {
+                    lastSeen[bit] = i;
+                }
+            }
+
+            int maxIndex = i;
+            for (int bit = 0; bit < 32; bit++) {
+                maxIndex = max(maxIndex, lastSeen[bit]);
+            }
+
+            ans[i] = maxIndex - i + 1;
+        }
+
+        return ans;
+    }
+};
