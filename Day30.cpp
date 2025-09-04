07955b45cbecc4514a2b2e1002b31b1b1cfb1d8b
Ankit Singh <2023pcecsankit019@poornima.org>
2025-09-04 19:51:16 +0530

    Update Day30.cpp

--- Day30.cpp
+++ Day30.cpp
@@ -9,3 +9,52 @@ Return the length of the longest such subarray.
 The bitwise AND of an array is the bitwise AND of all the numbers in it.
 
 A subarray is a contiguous sequence of elements within an array. */
+
+
+// class Solution {
+// public:
+//     int longestSubarray(vector<int>& nums) {
+//         int maxVal = 0, ans = 0, currentStreak = 0;
+
+//         for (int num : nums) {
+//             if (maxVal < num) {
+//                 maxVal = num;
+//                 ans = currentStreak = 0;
+//             }
+
+//             if (maxVal == num) {
+//                 currentStreak++;
+//             } else {
+//                 currentStreak = 0;
+//             }
+
+//             ans = max(ans, currentStreak);
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
+
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int n = nums.size();
+        int max_value = *max_element(nums.begin(),nums.end());
+        
+        int max_len = 0;
+        int curr_len = 0;
+        for(int i=0;i<n;++i){
+            if(nums[i]==max_value){
+                curr_len++;
+                if(max_len < curr_len)
+                    max_len = curr_len;
+            }else{
+                curr_len = 0;
+            }
+        }
+        return max_len;
+    }
+};
