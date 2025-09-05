01bfd1399affd610ddd87ec5105ecf7ed16e42af
Ankit Singh <2023pcecsankit019@poornima.org>
2025-09-05 19:38:59 +0530

    Update Day31.cpp

--- Day31.cpp
+++ Day31.cpp
@@ -59,3 +59,21 @@ public:
 
 
 // Method : 3
+class Solution {
+public:
+    int subarrayBitwiseORs(vector<int>& arr) {
+        unordered_set<int> result, current;
+
+        for(int num : arr){
+            unordered_set<int> next;
+            next.insert(num);
+            for(int val : current){
+                next.insert(val | num);
+            }
+            current = next;
+            result.insert(current.begin(), current.end());
+        }
+
+        return result.size();
+    }
+};
